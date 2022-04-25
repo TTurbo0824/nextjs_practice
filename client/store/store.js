73f7041355048837fcbe1c2934fa6854b257a8ba
Kyungjoo Ha <429why@gmail.com>
2022-04-25 20:15:50 +0900

    add: Hydrate action

--- client/store/store.js
+++ client/store/store.js
@@ -1,9 +1,5 @@
-import {
-  createStore,
-  applyMiddleware,
-  combineReducers,
-} from 'redux';
-import {createWrapper} from 'next-redux-wrapper';
+import {createStore, applyMiddleware, combineReducers} from 'redux';
+import {HYDRATE, createWrapper} from 'next-redux-wrapper';
 import {composeWithDevTools} from 'redux-devtools-extension';
 import users from './users/reducer';
 import counter from './counter/reducer';
@@ -13,8 +9,25 @@ const combinedReducers = combineReducers({
   users,
 });
 
+const masterReducer = (state, action) => {
+  if (action.type === HYDRATE) {
+    const nextState = {
+      ...state,
+      counter: {
+        count: state.counter.count + action.payload.counter.count,
+      },
+      users: {
+        users: [
+          ...new Set([...action.payload.users.users, ...state.users.users]),
+        ],
+      },
+    };
+    return nextState;
+  } else return combinedReducers(state, action);
+};
+
 const initStore = () => {
-  return createStore(combinedReducers, composeWithDevTools(applyMiddleware()));
+  return createStore(masterReducer, composeWithDevTools(applyMiddleware()));
 };
 
 export const wrapper = createWrapper(initStore);
