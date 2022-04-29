217b03be4ed55d089e94bda22f8d3300d49100ef
Kyungjoo Ha <429why@gmail.com>
2022-04-29 22:49:12 +0900

    add: New redux store

--- client/store/store.js
+++ client/store/store.js
@@ -1,33 +1,17 @@
-import {createStore, applyMiddleware, combineReducers} from 'redux';
-import {HYDRATE, createWrapper} from 'next-redux-wrapper';
-import {composeWithDevTools} from 'redux-devtools-extension';
-import users from './users/reducer';
-import counter from './counter/reducer';
+import {combineReducers, configureStore} from '@reduxjs/toolkit';
+import {createWrapper} from 'next-redux-wrapper';
+import counter from './counterSlice';
+import users from './usersSlice';
 
 const combinedReducers = combineReducers({
   counter,
   users,
 });
 
-const masterReducer = (state, action) => {
-  if (action.type === HYDRATE) {
-    const nextState = {
-      ...state,
-      counter: {
-        count: state.counter.count + action.payload.counter.count,
-      },
-      users: {
-        users: [
-          ...new Set([...action.payload.users.users, ...state.users.users]),
-        ],
-      },
-    };
-    return nextState;
-  } else return combinedReducers(state, action);
+export const makeStore = () => {
+  configureStore({
+    reducer: combinedReducers,
+  });
 };
 
-const initStore = () => {
-  return createStore(masterReducer, composeWithDevTools(applyMiddleware()));
-};
-
-export const wrapper = createWrapper(initStore);
+export const wrapper = createWrapper(makeStore);
